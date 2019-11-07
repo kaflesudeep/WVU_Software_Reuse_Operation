76b7d3441065b19e5aa41fdf15dacd30652293ef
Sudeep Kafle <kaflesudeepjava@gmail.com>
2019-11-07 13:36:52 -0500

    Update Operation.java

--- CalculatorAndConverter/src/CommonMethods/Operation.java
+++ CalculatorAndConverter/src/CommonMethods/Operation.java
@@ -117,4 +117,36 @@ public class Operation {
 		}
 	}
 
+	
+		public Object performExponent(Object num1, Converter first, Object num2, Converter second) {
+		// 1. convert all number to int
+		String firstNum, secondNum;
+		int resultInt;
+		double resultDouble;
+
+		try {
+			if (first.equals(Converter.DOUBLE)) {
+				firstNum = first.convertTo(Converter.DOUBLE, num1);
+				secondNum = second.convertTo(Converter.DOUBLE, num2);
+				resultDouble = Math.pow(Double.parseDouble(firstNum),  Double.parseDouble(secondNum));
+				DecimalFormat df = new DecimalFormat("###.##");
+				if (Double.parseDouble(secondNum) > 0) {
+					df.format(resultDouble);
+				}
+				return "" + resultDouble;
+
+			} else {
+				firstNum = first.convertTo(Converter.INTEGER, num1);
+				secondNum = second.convertTo(Converter.INTEGER, num2);
+				resultInt = (int) (resultDouble = Math.pow(Integer.parseInt(firstNum), Integer.parseInt(secondNum)));
+				return Converter.INTEGER.convertTo(first, resultInt);
+			}
+		} catch (Exception ex) {
+			System.out.println("Error Occurred: ");
+			System.out.println(ex.getMessage());
+			return "Infinity";
+		}
+
+	}
+	
 }
